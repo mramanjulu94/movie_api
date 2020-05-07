6782eba464ee227f1229b036620cf781e168c32c
mramanjulu94 <53407664+mramanjulu94@users.noreply.github.com>
2020-05-07 18:04:29 -0400

    update index

--- index.js
+++ index.js
@@ -1,87 +1,108 @@
 const express = require('express');
 
-morgan =require('morgan');
+morgan = require('morgan');
+
+
 const app = express();
-
-let topMovies = [ {
-  movie : 'The Dark Knight',
-
-},
-{
-  movie : 'Django Unchained',
-
-},
-{
-  movie : 'Crazy Stupid Love',
-
-}
-
-{
-  movie : 'Rush Hour',
-
-}
-{
-  movie : 'Spider-Man 2',
-
-}
-{
-  movie : 'Avengers Infinity War',
-
-}
-{
-  movie : 'Bad Boys',
-
-}
-{
-  movie : 'Space Jam',
-
-}
-{
-  movie : 'Lion King',
-
-}
-{
-  movie : 'Inception',
-
-}
-]
-
-// GET requests
-app.get('/', function(req, res) {
-  res.send('Welcome to my movie club!')
-});
-app.get('/documentation', function(req, res) {
-  res.sendFile('public/Documentation.html', { root : __dirname });
-});
-app.get('/movies', function(req, res) {
-  res.json(topMovies)
-});
-
-
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Welcome to my movie club!\n');
-
-
-// listen for requests
-app.listen(8080, () =>
-  console.log('Your app is listening on port 8080.')
-);
-
-
 
 
 app.use(morgan('common'));
 
-app.get('/', function (req, res) {
-  res.send('Welcome to my app!');
-});
-app.get('/secreturl', function (req, res) {
-  res.send('This is a secret url with super top-secret content.');
+
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+
+let topMovies = [
+  {
+    title: 'The Dark Knight',
+  },
+    {
+      title: 'The Bourne Identity',
+    },
+    {
+      title: 'Lord of the Rings',
+    },
+    {
+      title: 'Slumdog Millionaire',
+    },
+    {
+      title: 'The Godfather',
+    },
+    {
+      title: 'Django Unchained',
+    },
+    {
+      title: 'Insidious',
+    },
+    {
+      title: 'The Conjuring',
+    },
+    {
+      title: 'Rush Hour',
+    },
+    {
+      title: 'The Interview',
+    },
+];
+
+
+
+let myLogger = (req, res, next) => {
+  console.log(req.url);
+  next();
+};
+
+
+let requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
+
+
+
+app.use(myLogger);
+app.use(requestTime);
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  // logic
 });
 
-app.listen(8080);
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+
+// GET requests
+app.get('/', (req, res) => {
+  let responseText = 'Welcome to my app!';
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
 });
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+
+app.get('/secreturl', (req, res) => {
+  let responseText = 'This is a secret url with super top-secret content.';
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
+
+});
+
+
+// listen for requests
+app.listen(8080, () =>
+  console.log('Your app is listening on port 8080.');
+);
